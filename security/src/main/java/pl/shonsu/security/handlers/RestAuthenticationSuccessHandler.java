b63934084ab604668def2fa397d8a52edff775c2
Shonsu <shonsu82@gmail.com>
2023-05-08 14:53:47 +0200

    claims for jwt token

--- security/src/main/java/pl/shonsu/security/handlers/RestAuthenticationSuccessHandler.java
+++ security/src/main/java/pl/shonsu/security/handlers/RestAuthenticationSuccessHandler.java
@@ -7,12 +7,17 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import pl.shonsu.user.model.User;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Component
 public class RestAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
@@ -23,13 +28,22 @@ public class RestAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuc
     @Value("${app.security.jwt.secret}")
     private String secret;
 
+    private final UserDetailsService userDetailsService;
+
+    public RestAuthenticationSuccessHandler(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
                                         HttpServletResponse response,
                                         Authentication authentication) throws IOException, ServletException {
         UserDetails principal = (UserDetails) authentication.getPrincipal();
+        String username = principal.getUsername();
+        UserDetails user = userDetailsService.loadUserByUsername(username);
         String token = JWT.create()
-                .withSubject(principal.getUsername())
+                .withSubject(username)
+                .withClaim("Roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList())
                 .withExpiresAt(new Date(System.currentTimeMillis() + expirationTime))
                 .sign(Algorithm.HMAC256(secret));
 
